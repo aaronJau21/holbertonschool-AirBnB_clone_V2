a68b78eb7447c8362516f772b5100d62733d06e3
aaronlimadigital <101225802+aaronJau21@users.noreply.github.com>
2023-02-26 00:22:49 -0500

    Test and Models

---
+++ models/base_model.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Define a class BaseModel to support other classes.
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """ Represent support for other classes. """
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor method
+        Parameters:
+            args (tuple):
+                Not intented to be used. If passed, it will be skiped
+            kwargs (dict):
+                The keys of this dicionary are the name attributes and
+                the values are the values of each attribute
+        """
+
+        if kwargs:
+            """
+            This block is intended to RELOAD an object,
+            thats why it does not call the storage.new method
+            i.e is not added to the dicionary that saves the
+            objects created
+            """
+
+            del kwargs["__class__"]
+            kwargs["created_at"] = datetime.fromisoformat(kwargs["created_at"])
+            kwargs["updated_at"] = datetime.fromisoformat(kwargs["updated_at"])
+            self.__dict__.update(**kwargs)
+        else:
+            """
+            If not kwargs provided, it means that a new object is being
+            created, therefore it must be added to the dictionary
+            storage.new
+            """
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """ String representation of the instances """
+
+        str = "[{}] ({}) {}".format(type(self).__name__,
+                                    self.id, self.__dict__)
+        return (str)
+
+    def save(self):
+        """ Saves with the actual time """
+
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ Returns a dictionary containing all keys/values. """
+
+        n_dict = self.__dict__.copy()
+        n_dict["__class__"] = self.__class__.__name__
+        if hasattr(self, "created_at"):
+            n_dict["created_at"] = n_dict["created_at"].isoformat()
+
+        if hasattr(self, "updated_at"):
+            n_dict["updated_at"] = n_dict["updated_at"].isoformat()
+
+        return (n_dict)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" This module defines the class FileStorage """
+
+import json
+import os
+import models
+
+
+class FileStorage():
+    """
+    Class that serializes instances to a JSON file and deserializes
+    JSON file to instances
+    Private Class Attributes:
+        __file_path (str):
+            path to JSON file (ex: file.json)
+        __objects (dict):
+            empty dictionary that will store all objects. The format
+            will be as follows: <class name>.id (ex: to store a
+            BaseModel object with id=12121212, the key will be
+            BaseModel.id)
+    Public Instance Methods:
+        all(self):
+            returns the dictionary __objects
+        new(self, obj):
+            sets in __objects the obj with key <obj class> name.id
+        save(self):
+            serializes __objects to the JSON file (path: __file_path)
+        reload(self):
+            deserializes the JSON file to __objects
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns a dictionary as follows:
+            key (<class name>.id) : value (obj)
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """ Sets in __objects the obj with key <obj class name>.id
+        Parameters:
+            obj (any):
+                object to be stored in __objects dictionary
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects dictionary to JSON string and stores in
+        __file_path
+        """
+        objects_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(objects_dict, file, indent=4, sort_keys=True)
+
+    def reload(self):
+        """ Deserializes the JSON file to __objects
+        If the JSON file (__file_path) exists, deserializes the JSON
+        file to __objects dictionary. Otherwise, does nothing, and no exception
+        will be raised
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                dict_loaded = json.load(file)
+                objects_loaded = {k: models.classes[v["__class__"]](**v)
+                                  for k, v in dict_loaded.items()}
+                self.__objects.update(**objects_loaded)
