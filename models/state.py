64db471ae62586b4f248f7ad67343fb0f952a0d1
aaronJau21 <aaronjau21@gmail.com>
2023-04-08 00:18:05 -0500

    state

--- models/state.py
+++ models/state.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
-""" This module defines the State class """
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+import os
+import models
 
-from .base_model import BaseModel
 
+class State(BaseModel, Base):
+    """ State class """
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-class State(BaseModel):
-    """ A class representing an state """
-    name = ""
+        def __init__(self, *args, **kwargs):
+            """initializes state"""
+            super().__init__(*args, **kwargs)
+
+        @property
+        def cities(self):
+            """return a list of city instances with state_id = current"""
+            all_instances = models.storage.all(City)
+            query = []
+            for key, value in all_instances.items():
+                if getattr(value, 'state_id') == self.id:
+                    query.append(value)
+            return query
